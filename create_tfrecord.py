f5efafca787251afba39cd528400551f7f17d3d1
ronen-halevy <ronen567@gmail.com>
2022-09-23 23:27:57 +0300

    support splitted input

--- create_tfrecord.py
+++ create_tfrecord.py
@@ -104,7 +104,7 @@ def write_tfrecord_file(tfrecord_idx, image_entries, annotations_list, images_di
     ) as writer:
         for image_entry in image_entries:
             img_annots = [anno for anno in annotations_list if anno['image_id'] == image_entry['id']]
-            img_path = +f'{images_dir}/{image_entry["file_name"]}'
+            img_path = f'{images_dir}/{image_entry["file_name"]}'
             image = tf.io.decode_jpeg(tf.io.read_file(img_path))
             img_width = image_entry['width']
             img_height = image_entry['height']
@@ -112,11 +112,10 @@ def write_tfrecord_file(tfrecord_idx, image_entries, annotations_list, images_di
             writer.write(example.SerializeToString())
 
 def create_tfrecords(input_annotations_file,
-                     images_dir,
-                     tfrecords_out_dir,
+                     base_input_dir,
+                     out_dir,
+                     splits,
                      optimal_file_size,
-                     train_split,
-                     val_split,
                      classes_name_file,
                      examples_limit=None):
     """
@@ -136,52 +135,45 @@ def create_tfrecords(input_annotations_file,
     """
     class_names = [c.strip() for c in open(classes_name_file).readlines()]
 
+    for split in splits:
+        out_dir = f'{out_dir}/{split}'
+        input_dir = f'{base_input_dir}/{split}'
+        annotations_filename = f'{input_dir}/{input_annotations_file}'
 
-    train_dir = f'{tfrecords_out_dir}/train'
-    val_dir = f'{tfrecords_out_dir}/val'
-    test_dir = f'{tfrecords_out_dir}/test'
-
-    for out_dir in [train_dir, val_dir, test_dir]:
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
         else:
             to_del_files = glob.glob(f'{out_dir}/*.tfrec')
             [os.remove(f) for f in to_del_files]
+        with open(annotations_filename, 'r') as f:
+            annotations = json.load(f)
 
-    with open(input_annotations_file, 'r') as f:
-        annotations = json.load(f)
+        annotations_list = annotations['annotations']
+        num_examples = min(len(annotations['images']), examples_limit or float('inf'))
+        images_list = annotations['images'][0:num_examples]
+        categories_list = annotations['categories']
+        sparse_to_dense_category_id = {category['id']: index for index, category in enumerate(categories_list)}
 
-    annotations_list = annotations['annotations']
-    num_examples = min(len(annotations['images']), examples_limit or float('inf'))
-    images_list = annotations['images'][0:num_examples]
-    categories_list = annotations['categories']
-    sparse_to_dense_category_id = {category['id']: index for index, category in enumerate(categories_list)}
 
-    # split dataset:
-    train_images_list, remainder = np.split(images_list, [int(train_split * len(images_list))])
-    val_images_list, test_images_list = np.split(remainder, [int((val_split) * len(images_list))])
+        num_images_in_tfrecord_file = calc_num_images_in_tfrecord_file(images_list, input_dir, optimal_file_size)
 
-    num_images_in_tfrecord_file = calc_num_images_in_tfrecord_file(images_list, images_dir, optimal_file_size)
-
-    for split_images_list, out_dir in zip([train_images_list, val_images_list, test_images_list], [train_dir, val_dir, test_dir]):
-        # find number of tfrec files needed:
-        num_tfrecords_files = int(math.ceil(len(split_images_list) / num_images_in_tfrecord_file))
-        print(f'Starting! \nCreating {len(split_images_list)} examples in {num_tfrecords_files} tfrecord files.')
-        print(f'Output dir: {tfrecords_out_dir}')
+        num_tfrecords_files = int(math.ceil(len(images_list) / num_images_in_tfrecord_file))
+        print(f'Starting for {split} split! \nCreating {len(images_list)} examples in {num_tfrecords_files} tfrecord files.')
+        print(f'Output dir: {out_dir}')
         start_record = 0
         # calc mum of images per tfrec:
-        tfrec_files_sizes = np.tile([len(split_images_list) / num_tfrecords_files], num_tfrecords_files).astype(int)
+        tfrec_files_sizes = np.tile([len(images_list) / num_tfrecords_files], num_tfrecords_files).astype(int)
         # spill entries remainder to last tfrecord file:
-        tfrec_files_sizes[-1] = tfrec_files_sizes[-1] + (len(split_images_list) -  num_tfrecords_files *sum(tfrec_files_sizes))
+        tfrec_files_sizes[-1] = tfrec_files_sizes[-1] + (len(images_list) -  num_tfrecords_files *sum(tfrec_files_sizes))
         for tfrecord_idx, tfrec_file_size in enumerate(tfrec_files_sizes):
-            write_tfrecord_file(tfrecord_idx, split_images_list[start_record: tfrec_file_size], annotations_list, images_dir, out_dir, class_names, sparse_to_dense_category_id)
+            write_tfrecord_file(tfrecord_idx, images_list[start_record: tfrec_file_size], annotations_list, input_dir, out_dir, class_names, sparse_to_dense_category_id)
             start_record = start_record + tfrec_file_size
 
     # save a copy of class names file here:
-    class_name_path = f'{tfrecords_out_dir}/class.names'
-    with open(class_name_path, 'w') as f:
-        for class_name in class_names:
-            f.write("%s\n" % class_name)
+        class_name_path = f'{out_dir}/class.names'
+        with open(class_name_path, 'w') as f:
+            for class_name in class_names:
+                f.write("%s\n" % class_name)
 
 def main():
     parser = argparse.ArgumentParser()
